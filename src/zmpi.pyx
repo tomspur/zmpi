3e31b0f890fb700f6172e1c16e7c4038d2ebde59
Thomas Spura <thomas.spura@gmail.com>
2013-02-24 15:58:50 +0100

    Fix Licensing Header to reflect starting date of project

--- src/zmpi.pyx
+++ src/zmpi.pyx
@@ -1,19 +1,19 @@
-#  Copyright (c) 2011 Thomas Spura
+# Copyright (c) 2013 Thomas Spura
 #
-#  This file is part of ZMPI.
+# This file is part of ZMPI.
 #
-#  ZMPI is free software; you can redistribute it and/or modify it under
-#  the terms of the GNU Lesser General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  (at your option) any later version.
+# ZMPI is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
 #
-#  ZMPI is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Lesser General Public License for more details.
+# ZMPI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
 #
-#  You should have received a copy of the GNU Lesser General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 
