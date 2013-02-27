1741a9aa2b6ba5a97ca074474b4c72027e9ca6a0
Thomas Spura <thomas.spura@gmail.com>
2013-02-27 01:03:12 +0100

    Subscribe the clients to Master

--- src/zmpi/communication.pyx
+++ src/zmpi/communication.pyx
@@ -40,6 +40,8 @@ cdef class Client(Communication):
         print "Calling Client.__init__"
         self.rank = int(os.environ.get("ZMPI_RANK", "0"))
         self.size = int(os.environ.get("ZMPI_SIZE", "0"))
+        self.sock_sub = self.context.socket(zmq.SUB)
+        self.sock_sub.connect(os.environ["ZMPI_PORT_SUB"])
 
     def __del__(self):
         print "Calling Client.__del__"
@@ -67,7 +69,7 @@ cdef class Master(Communication):
             envs.append(os.environ.copy())
             envs[i]["ZMPI_RANK"] = str(i)
             envs[i]["ZMPI_SIZE"] = str(self.size)
-            envs[i]["ZMPI_PORT_SUB"] = str(self.port_pub)
+            envs[i]["ZMPI_PORT_SUB"] = "tcp://127.0.0.1:%d"%(self.port_pub)
 
         if DEBUG:
             print "Starting %d processes."% self.size
